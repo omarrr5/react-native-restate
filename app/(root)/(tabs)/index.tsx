0ef27655732888515bb5569a6a5e1ee897e07248
omar tarik yehya <khalaf.omar20@gmail.com>
2025-02-23 12:16:55 +0200

    feat: home screen flatlists

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,64 +1,80 @@
 import images from "@/constants/images";
 import icons from "@/constants/icons";
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, TouchableOpacity, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Search from "@/components/Search";
 import { Cards, FeaturedCards } from "@/components/Cards";
 import Filters from "@/components/Filters";
+import { useGlobalContext } from "@/lib/global-provider";
 
 export default function Index() {
+  const {user} = useGlobalContext();
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5">
-        <View className="flex flex-row items-center justify-between mt-5">
-          <View className="flex flex-row items-center">
-            <Image source={images.avatar} className="size-12 rounded-full" />
-            <View className="flex flex-col items-start ml-2 justify-center">
-              <Text className="text-xs font-rubik text-black-100">
-                Good Morning
-              </Text>
-              <Text className="text-base  font-rubik-medium text-black-300">
-                Omar{" "}
-              </Text>
+      <FlatList
+        data={[1, 2, 3, 4]}
+        renderItem={({ item }) => <Cards />}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row items-center">
+                <Image
+                  source={{uri: user?.avatar}}
+                  className="size-12 rounded-full"
+                />
+                <View className="flex flex-col items-start ml-2 justify-center">
+                  <Text className="text-xs font-rubik text-black-100">
+                    Good Morning
+                  </Text>
+                  <Text className="text-base  font-rubik-medium text-black-300">
+                    {user?.name}{" "}
+                  </Text>
+                </View>
+              </View>
+              <Image source={icons.bell} className="size-6" />
             </View>
-          </View>
-          <Image source={icons.bell} className="size-6" />
-        </View>
-        <Search />
-        <View className="my-5">
-          <View className="flex flex-row items-center justify-between">
-            <Text className="text-xl font-rubik-bold text-black-300">
-              Featured
-            </Text>
-            <TouchableOpacity>
-              <Text className="text-base font-rubik-bold text-primary-300">
-                See All
-              </Text>
-            </TouchableOpacity>
-          </View>
+            <Search />
+            <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                  Featured
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-primary-300">
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </View>
 
-          <View className="flex flex-row gap-5 mt-5">
-            <FeaturedCards/>
-            <FeaturedCards/>
-            <FeaturedCards/>
-          </View>
-        </View>
-        <View className="flex flex-row items-center justify-between">
-            <Text className="text-xl font-rubik-bold text-black-300">
-              Our Recommendation
-            </Text>
-            <TouchableOpacity>
-              <Text className="text-base font-rubik-bold text-primary-300">
-                See All
+              <FlatList
+                data={[1, 2, 3]}
+                renderItem={({ item }) => <FeaturedCards />}
+                keyExtractor={(item) => item.toString()}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                bounces={false}
+                contentContainerClassName="flex gap-5 mt-5"
+              />
+            </View>
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Our Recommendation
               </Text>
-            </TouchableOpacity>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <Filters />
           </View>
-          <Filters/>
-          <View className="flex flex-row gap-5 mt-5">
-            <Cards/>
-            <Cards/>
-          </View>
-      </View>
+        }
+      />
     </SafeAreaView>
   );
 }
